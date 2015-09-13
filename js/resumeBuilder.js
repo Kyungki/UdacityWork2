5059d853d78276e165c7cd38118337537ca67db3
Kyungki <kkim369@gatech.edu>
2015-09-13 14:21:35 -0400

    Resume formatted version 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,15 @@
 
+
+
+
 var bio = 
 {
 	"name" : "Kyungki Kim",
 	"role" : "Web Developer",
 	"contacts" : {
+		"mobile" : "770-361-2262",
+		"email" : "kkim369@gatech.edu",
+		"location" : "Atlanta, Georgia"
 
 	},
 	"welcomeMessage" : "Welcome to Kyungki Kim's webpage",
@@ -72,6 +78,7 @@ var works =
 			"employer" : "Civil Engineering, Georgia Institute of Technology",
 			"title" : "Graduate research assistant",
 			"dates" : "August 2011 - present",
+			"location" : "Atlanta, Georgia",
 			"description" : "Developed simulation programs for construction safety checking. Used APIs of commercial software programs (Autodesk Revit, Tekla Structures, etc.) and developed my own Java construction simulator with viewer."
 		}
 	]
@@ -106,6 +113,59 @@ var projects =
 			]
 		},
 	]
+}
+
+
+//add information to header
+var formattedName = HTMLheaderName.replace ("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace ("%data%", bio.role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+//add contact information
+var formattedContactEmail = HTMLemail.replace ("%data%", bio.contacts.email);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedMobile = HTMLlocation.replace("%data%", bio.contacts.location);
+
+
+$("#footerContacts").append(formattedContactEmail);
+$("#footerContacts").append(formattedMobile);
+$("#footerContacts").append(formattedMobile);
+
+//add work experience
+
+for (i = 0; i < works.jobs.length; i++)
+{
+	var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", works.jobs[i].employer);
+	var formattedWorkTitle = HTMLworkTitle.replace("%data%", works.jobs[i].title);
+	var formattedWorkDates = HTMLworkDates.replace("%data%", works.jobs[i].dates);
+	var formattedWorkLocation = HTMLworkLocation.replace("%data%", works.jobs[i].location);
+	var formattedWworkDescription = HTMLworkDescription.replace("%data%", works.jobs[i].description);
+
+	$("#workExperience").append(HTMLworkStart);
+	$("#workExperience").append(formattedWorkEmployer);
+	$("#workExperience").append(formattedWorkTitle);
+	$("#workExperience").append(formattedWorkDates);
+	$("#workExperience").append(formattedWorkLocation);
+	$("#workExperience").append(formattedWworkDescription);
+}
+
+
+//add education
+
+for (i = 0; i < education.schools.length; i++)
+{
+	var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name);
+	var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+	var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+	var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].city);
+	var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
+
+	$("#education").append(formattedSchoolName);
+	$("#education").append(formattedSchoolDegree);
+	$("#education").append(formattedSchoolDates);
+	$("#education").append(formattedSchoolLocation);
+	$("#education").append(formattedschoolMajor);
 }
 
 
